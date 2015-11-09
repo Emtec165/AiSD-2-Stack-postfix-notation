7ca0cdeb29e5f53c8100d29768a192445356ac29
Emtec165 - Krzysztof Pikóra <krzysztof.pikora@gmail.com>
2015-11-09 15:07:48 +0100

    Stack

--- Python_AiSD_Stosy.py
+++ Python_AiSD_Stosy.py
@@ -1,13 +1,36 @@
-x=5
-print(3 and x==5)
+﻿class Stack:
+    #variable
+    __stack = []
 
-print("ola fernandez!!!!", end = " ")
+    #methods
+    def __init__(self):
+        self.__stack= []
 
+    def push(self, object):
+        self.__stack.append(object)
 
-while(x<=10):
-    if(x%2==0):
-        print(x)
-        x+=1
-    else:
-        x+=1
-        continue
+    def pop(self):
+        return self.__stack.pop()
+
+    def peek(self):
+        return self.__stack[-1]
+
+    def isEmpty(self):
+        if not self.__stack:
+            return True
+        else:
+            return False
+
+    def size(self):
+        return len(self.__stack)
+
+stos = Stack()
+stos.push(4)
+stos.push(3)
+stos.push(2)
+stos.push(1)
+print(stos.pop())
+print(stos.peek())
+print(stos.peek())
+print(stos.isEmpty())
+print(stos.size())
